d78312952d22bdc1fbea1a9fd85eca45bc20fe51
rlogicaltechsoft <rahul@rlogical.com>
2020-12-09 15:14:45 +0530

    Add project files.

---
+++ CoreMVC/Interfaces/IEmployee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Interfaces
+{
+    interface IEmployee
+    {
+         public List<string> Employees { get; set; }
+    }
+}
---
+++ CoreMVC/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using CoreMVC.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVC.Models
+{
+    public class EmployeeModel : IEmployee
+    {
+        public int Id { get; set; }
+        public string Employee_Name { get; set; }
+        public List<string> Employees { get; set; }
+    }
+
+}
